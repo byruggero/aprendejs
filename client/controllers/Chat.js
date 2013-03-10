ad8d76164b2e91ac11e60fe85c66d1280f2fd858
jose armando gonzalez lopez <iam@armando.mx>
2013-03-10 16:45:10 -0600

    se agreo al twitter coneect

---
+++ client/controllers/Chat.js
@@ -0,0 +1,14 @@
+// Template.chat.events({
+//     // logearse con twitter
+//     'click #login_btn': function (e, tpl) {
+//         Meteor.loginWithTwitter({
+//             //requestPermisions: ['email', 'user_about_me', 'profile', 'user_likes']
+//         }, function (err) {
+//             if (err) {
+//                 //handling login error
+//             } else {
+//                 //show alert
+//             }
+//         });
+//     }
+// });
---
+++ server/config.js
@@ -0,0 +1,9 @@
+// Accounts.loginServiceConfiguration.remove({
+//   service: "twitter"
+// });
+
+// Accounts.loginServiceConfiguration.insert({
+//   service: "twitter",
+//   appId: 'SBdsNROyRbpQP2Bw6beUzw',
+//   secret: 'V0CnmFNQSVE1AeFaWUAuVOjclGmkiGyJII3PIYtDDo'
+// });
