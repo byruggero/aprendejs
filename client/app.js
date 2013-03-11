eef8cdff58b4914c868c5c7f9a60e404a88f3aed
Crysfel Villa <crysfel@bleext.com>
2013-03-11 01:32:44 -0600

    Editor de código

--- client/app.js
+++ client/app.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$().UItoTop({ easingType: 'easeOutQuart' });
+
+    $("#toggleEditor").live("click", function(){
+        $('#editor').slideToggle('slow');
+        return false;
+    });
 });
--- client/js/main.js
+++ client/js/main.js
@@ -165,35 +165,29 @@
 		
 		//Nivo slider customization
         $(window).load(function() {
-    	$('#slider').nivoSlider({
-        effect:'fade', //Specify sets like: 'fold,fade,sliceDown'
-        slices:20,
-        animSpeed:800, //Slide transition speed
-        pauseTime:5000,
-        startSlide:0, //Set starting Slide (0 index)
-        directionNav:true, //Next & Prev
-        directionNavHide:true, //Only show on hover
-        controlNav:false, //1,2,3...
-        controlNavThumbs:false, //Use thumbnails for Control Nav
-        controlNavThumbsFromRel:false, //Use image rel for thumbs
-        controlNavThumbsSearch: '.jpg', //Replace this with...
-        controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
-        keyboardNav:true, //Use left & right arrows
-        pauseOnHover:true, //Stop animation while hovering
-        manualAdvance:false, //Force manual transitions
-        captionOpacity:0.8, //Universal caption opacity
-        beforeChange: function(){},
-        afterChange: function(){},
-        slideshowEnd: function(){}, //Triggers after all slides have been shown
-        lastSlide: function(){}, //Triggers when last slide is shown
-        afterLoad: function(){} //Triggers when slider has loaded
-    	});
+		$('#slider').nivoSlider({
+			effect:'fade', //Specify sets like: 'fold,fade,sliceDown'
+			slices:20,
+			animSpeed:800, //Slide transition speed
+			pauseTime:5000,
+			startSlide:0, //Set starting Slide (0 index)
+			directionNav:true, //Next & Prev
+			directionNavHide:true, //Only show on hover
+			controlNav:false, //1,2,3...
+			controlNavThumbs:false, //Use thumbnails for Control Nav
+			controlNavThumbsFromRel:false, //Use image rel for thumbs
+			controlNavThumbsSearch: '.jpg', //Replace this with...
+			controlNavThumbsReplace: '_thumb.jpg', //...this in thumb Image src
+			keyboardNav:true, //Use left & right arrows
+			pauseOnHover:true, //Stop animation while hovering
+			manualAdvance:false, //Force manual transitions
+			captionOpacity:0.8, //Universal caption opacity
+			beforeChange: function(){},
+			afterChange: function(){},
+			slideshowEnd: function(){}, //Triggers after all slides have been shown
+			lastSlide: function(){}, //Triggers when last slide is shown
+			afterLoad: function(){} //Triggers when slider has loaded
 		});
-
-			
-$(document).ready(function() { 
-				
-}); 		
-		
+});
 
 
